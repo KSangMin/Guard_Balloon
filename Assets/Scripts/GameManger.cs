32d3f3822150c090ffe4e101773fe7295faeed44
KSangMin <fhfpszmfnwm@naver.com>
2025-01-21 09:51:41 +0900

    시간 흐르는 기능
    시간 흐르는 기능

--- Assets/Scripts/GameManger.cs
+++ Assets/Scripts/GameManger.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManger : MonoBehaviour
 {
     public GameObject obstacle;
-    // Start is called before the first frame update
+
+    private float _time;
+    public Text timeText;
+
     void Start()
     {
         InvokeRepeating("CreateObstacle", 0f, 1f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _time += Time.deltaTime;
+        timeText.text = _time.ToString("N1");
     }
 
     public void CreateObstacle()
