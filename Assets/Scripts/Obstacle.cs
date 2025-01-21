fb4d7a826c6d6d7413d5ce355a499345a8cbfcc6
KSangMin <fhfpszmfnwm@naver.com>
2025-01-21 09:46:44 +0900

    장애물 랜덤 생성 구현
    장애물 랜덤 생성 구현

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = Random.Range(-3f, 3f);
+        float y = Random.Range(3f, 5f);
+        float scale = Random.Range(0.5f, 1.5f);
+
+        transform.position = new Vector2(x, y);
+        transform.localScale = new Vector2(scale, scale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
